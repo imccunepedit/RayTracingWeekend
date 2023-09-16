fb17e55739066002c1ef499b6ec31b8c1c026fd8
Ilya McCune-Pedit <ilyamcnpdt@protonmail.com>
2023-09-16 22:58:28 +0200

    implement rays!!

--- main.cpp
+++ main.cpp
@@ -6,20 +6,51 @@
 
 #include "color.h"
 #include "vec3.h"
+#include "ray.h"
 
 #include <iostream>
 
 
+color ray_color(const ray& r) {
+  vec3 unit_direction = normalize(r.direction());
+  // the y coord of our direction goes from -1 to 1 but lerp needs from 0 to 1
+  auto a = 0.5*(unit_direction.y() + 1.0);
+  return (1.0-a) * color(1.0, 1.0, 1.0) + a*color(0.5, 0.7, 1.0);
+}
 
 int main() {
 
-  // image
+  // Image
 
-  int image_width = 256;
-  int image_height = 256;
+  auto aspect_ratio = 16.0/9.0;
+  int image_width = 400;
+
+  // calculate the image height based on the widht and aspect ratio
+  int image_height = static_cast<int>(image_width/aspect_ratio);
+
+  // Camera
+  auto focal_length = 1.0;
+  auto camera_center = point3(0,0,0);
+
+  // set width and height based on the image since maybe the integer rounding casued them to skew from the aspect ratio
+  auto viewport_height = 2.0;
+  auto viewport_width = viewport_height * (static_cast<double>(image_width)/image_height);
+
+  // vectors define the viewport edges, v needs to be negative because 3d coords define y increasing up but image has zero at top
+  auto viewport_u = vec3(viewport_width, 0 ,0);
+  auto viewport_v = vec3(0, -viewport_height, 0);
+
+  // calculate the pixle sizes
+  auto pixel_delta_u = viewport_u / image_width;
+  auto pixel_delta_v = viewport_v / image_height;
+
+  // find the position of the top left pixel
+  auto viewport_upper_left = camera_center
+                           - vec3(0,0,focal_length) - (viewport_u + viewport_v)/2;
+  auto pixel00_loc = viewport_upper_left + (pixel_delta_u + pixel_delta_v)/2;
 
 
-  // render
+  // Render
 
   // P3 means asci color
   // then set image width and height and max color
@@ -31,7 +62,18 @@ int main() {
     // print to standard log how many lines of the image still need to be renderd
     std::clog << "\rScanlines remaining: " << (image_height -j) << " " << std::flush;
     for (int i = 0; i < image_width; i++) {
-      color pixel_color = color(double(i)/(image_width-1), double(j)/(image_height-1), 0);
+
+      // find postion of pixel in 3D
+      auto pixel_center = pixel00_loc + (i * pixel_delta_u) + (j * pixel_delta_v);
+
+      // get direction of ray from camera to pixel
+      auto ray_direction = pixel_center - camera_center;
+
+      // construct a ray and get color of it
+      ray r(camera_center, ray_direction);
+      color pixel_color = ray_color(r);
+
+      // write color to screen
       write_color(std::cout, pixel_color);
     }
   }
--- vec3.h
+++ vec3.h
@@ -11,59 +11,59 @@ class vec3 {
 
   public:
 
-  // array of 3 values, storing the elements of our vector
-  double e[3];
+    // array of 3 values, storing the elements of our vector
+    double e[3];
 
-  // constructor definitions for an empty vec3
-  vec3() : e{0,0,0} {}
-  // and a vec 3 with 3 values specified
-  vec3(double e0, double e1, double e2) : e{e0, e1, e2} {};
-
-
-  // expose the three elements of our vector as x, y, and z
-  double x() const { return e[0]; }
-  double y() const { return e[1]; }
-  double z() const { return e[2]; }
-
-  // return a negative vec3
-  vec3 operator-() const { return vec3(-e[0], -e[1], -e[2]); }
-
-  // return the value of the vec3 at the given index
-  double operator[](int i) const { return e[i]; }
-  // reference to the returned value this is just what the operator is expecting in some other cases
-  double& operator[](int i) { return e[i]; }
+    // constructor definitions for an empty vec3
+    vec3() : e{0,0,0} {}
+    // and a vec 3 with 3 values specified
+    vec3(double e0, double e1, double e2) : e{e0, e1, e2} {};
 
 
-  // add another vector onto this one
-  vec3& operator+=(const vec3 &v) {
-    e[0] += v.e[0];
-    e[1] += v.e[1];
-    e[2] += v.e[2];
-    return *this;
-  }
+    // expose the three elements of our vector as x, y, and z
+    double x() const { return e[0]; }
+    double y() const { return e[1]; }
+    double z() const { return e[2]; }
 
-  // sclae this vector by some amount
-  vec3& operator*=(const double t) {
-    e[0] += t;
-    e[1] += t;
-    e[2] += t;
-    return *this;
-  }
+    // return a negative vec3
+    vec3 operator-() const { return vec3(-e[0], -e[1], -e[2]); }
 
-  // we can use the *= operator to implement division
-  vec3& operator/=(double t) {
-    return *this *= 1/t;
-  }
+    // return the value of the vec3 at the given index
+    double operator[](int i) const { return e[i]; }
+    // reference to the returned value this is just what the operator is expecting in some other cases
+    double& operator[](int i) { return e[i]; }
 
 
-  // length uses sqrt which is taxing so we define length_squared elsewhere
-  double length() const {
-    return sqrt(length_squared());
-  }
+    // add another vector onto this one
+    vec3& operator+=(const vec3 &v) {
+      e[0] += v.e[0];
+      e[1] += v.e[1];
+      e[2] += v.e[2];
+      return *this;
+    }
 
-  double length_squared() const {
-    return e[0]*e[0] + e[1]*e[1] + e[2]*e[2];
-  }
+    // sclae this vector by some amount
+    vec3& operator*=(const double t) {
+      e[0] += t;
+      e[1] += t;
+      e[2] += t;
+      return *this;
+    }
+
+    // we can use the *= operator to implement division
+    vec3& operator/=(double t) {
+      return *this *= 1/t;
+    }
+
+
+    // length uses sqrt which is taxing so we define length_squared elsewhere
+    double length() const {
+      return sqrt(length_squared());
+    }
+
+    double length_squared() const {
+      return e[0]*e[0] + e[1]*e[1] + e[2]*e[2];
+    }
 };
 
 
