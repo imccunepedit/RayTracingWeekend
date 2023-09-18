d4aee22ca727c44ec4f31c57159a454676a78c11
Ilya McCune-Pedit <ilyamcnpdt@protonmail.com>
2023-09-18 10:21:33 +0200

    diffuse shading  with multiple bounces

--- vec3.h
+++ vec3.h
@@ -1,6 +1,7 @@
 #ifndef VEC3_H_
 #define VEC3_H_
 
+#include "rtweekend.h"
 #include <cmath>
 #include <iostream>
 
@@ -9,61 +10,59 @@ using std::sqrt;
 
 class vec3 {
 
-  public:
+public:
+  // array of 3 values, storing the elements of our vector
+  double e[3];
+  // constructor definitions for an empty vec3
+  vec3() : e{0,0,0} {}
+  // and a vec 3 with 3 values specified
+  vec3(double e0, double e1, double e2) : e{e0, e1, e2} {};
 
-    // array of 3 values, storing the elements of our vector
-    double e[3];
+  // expose the three elements of our vector as x, y, and z
+  double x() const { return e[0]; }
+  double y() const { return e[1]; }
+  double z() const { return e[2]; }
+  // return a negative vec3
+  vec3 operator-() const { return vec3(-e[0], -e[1], -e[2]); }
+  // return the value of the vec3 at the given index
+  double operator[](int i) const { return e[i]; }
+  // reference to the returned value this is just what the operator is expecting in some other cases
+  double& operator[](int i) { return e[i]; }
 
-    // constructor definitions for an empty vec3
-    vec3() : e{0,0,0} {}
-    // and a vec 3 with 3 values specified
-    vec3(double e0, double e1, double e2) : e{e0, e1, e2} {};
+  // add another vector onto this one
+  vec3& operator+=(const vec3 &v) {
+    e[0] += v.e[0];
+    e[1] += v.e[1];
+    e[2] += v.e[2];
+    return *this;
+  }
+  // sclae this vector by some amount
+  vec3& operator*=(const double t) {
+    e[0] += t;
+    e[1] += t;
+    e[2] += t;
+    return *this;
+  }
+  // we can use the *= operator to implement division
+  vec3& operator/=(double t) {
+    return *this *= 1/t;
+  }
 
+  // length uses sqrt which is taxing so we define length_squared elsewhere
+  double length() const {
+    return sqrt(length_squared());
+  }
+  double length_squared() const {
+    return e[0]*e[0] + e[1]*e[1] + e[2]*e[2];
+  }
 
-    // expose the three elements of our vector as x, y, and z
-    double x() const { return e[0]; }
-    double y() const { return e[1]; }
-    double z() const { return e[2]; }
+  static vec3 random() {
+    return vec3(random_double(), random_double(), random_double());
+  }
 
-    // return a negative vec3
-    vec3 operator-() const { return vec3(-e[0], -e[1], -e[2]); }
-
-    // return the value of the vec3 at the given index
-    double operator[](int i) const { return e[i]; }
-    // reference to the returned value this is just what the operator is expecting in some other cases
-    double& operator[](int i) { return e[i]; }
-
-
-    // add another vector onto this one
-    vec3& operator+=(const vec3 &v) {
-      e[0] += v.e[0];
-      e[1] += v.e[1];
-      e[2] += v.e[2];
-      return *this;
-    }
-
-    // sclae this vector by some amount
-    vec3& operator*=(const double t) {
-      e[0] += t;
-      e[1] += t;
-      e[2] += t;
-      return *this;
-    }
-
-    // we can use the *= operator to implement division
-    vec3& operator/=(double t) {
-      return *this *= 1/t;
-    }
-
-
-    // length uses sqrt which is taxing so we define length_squared elsewhere
-    double length() const {
-      return sqrt(length_squared());
-    }
-
-    double length_squared() const {
-      return e[0]*e[0] + e[1]*e[1] + e[2]*e[2];
-    }
+  static vec3 random(double min, double max) {
+    return vec3(random_double(min,max), random_double(min,max), random_double(min,max));
+  }
 };
 
 
@@ -71,9 +70,7 @@ class vec3 {
 using point3 = vec3;
 
 
-
 // vecotr functions
-
 
 // make << work nicely with vec3
 // not sure what inline is exactly for, something maybe about it not being defined in the class
@@ -133,5 +130,24 @@ inline vec3 unit_vector(vec3 v) {
   return v/v.length();
 }
 
+inline vec3 random_in_unit_sphere() {
+  while (true) {
+    auto p = vec3::random(-1,1);
+    if (p.length_squared() < 1) return p;
+  }
+}
+
+inline vec3 random_unit_vector() {
+  return unit_vector(random_in_unit_sphere());
+}
+
+
+inline vec3 random_on_hemisphere(const vec3& normal) {
+  vec3 on_unit_sphere = random_unit_vector();
+  if (dot(on_unit_sphere, normal) > 0.0)
+    return on_unit_sphere;
+  else
+    return -on_unit_sphere;
+}
 
 #endif // VEC3_H_
